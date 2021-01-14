50e9d9bf6a2df6180f4c7be7fc2ed6fb001de7c0
Tanghe Kevin <tanghekevin1@gmail.com>
2021-01-14 17:28:08 +0100

    exo finis

--- src/App.js
+++ src/App.js
@@ -4,21 +4,50 @@ import React, { Component } from 'react'
 export class App extends Component {
   
   changeTilte = () =>{
-    // e.target.innerHTML = "test"
-    document.querySelector("h1").innerHTML = "test"
+    this.setState({
+      h1: "test"
+    })
   }
 
   changeParagraphe = () => {
-    document.querySelector("p").innerHTML = "Ceci est un paragraphe changer"
+    this.setState({
+      p: "Test"
+    })
+  }
+
+  changeTilte2 = () => {
+    this.setState({
+      h1DeuxièmeTitre: document.querySelector("input").value
+    })
+  }
+
+  changeP2 = () => {
+    this.setState({
+      p2: document.querySelector("textarea").value
+    })
+  }
+
+  state = {
+    h1: "Titre",
+    h1DeuxièmeTitre: "Deuxième titre",
+    p: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni dolore quidem minima voluptate asperiores iste ratione ex atque, deserunt delectus dignissimos commodi, architecto dicta eveniet eius neque aliquam? Neque, saepe.",
+    p2: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
   }
 
   render() {
 
     return (
       <div>
-        <h1 onClick={this.changeTilte}>Titre</h1>
-        <p id="test">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni dolore quidem minima voluptate asperiores iste ratione ex atque, deserunt delectus dignissimos commodi, architecto dicta eveniet eius neque aliquam? Neque, saepe.</p>
+        <h1 onClick={this.changeTilte}>{this.state.h1}</h1>
+        <h1>{this.state.h1DeuxièmeTitre}</h1>
+        <p>{this.state.p}</p>
         <button onClick={this.changeParagraphe}>Modifier le paragraphe</button>
+        <div>
+          <input></input>
+          <button onClick={this.changeTilte2}>Go</button>
+        </div>
+        <p>{this.state.p2}</p>
+        <textarea onInput={this.changeP2}></textarea>
       </div>
     )
   }
